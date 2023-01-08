7cf262d5ff8f6c37e829134c2b1e41e618233102
chezydan <chezydan@users.noreply.github.com>
2023-01-08 19:45:40 +0000

    presets with function

--- script.js
+++ script.js
@@ -6,31 +6,66 @@ const btnStop=document.getElementById("stop")
 const timerImp = document.getElementById("timerImp")
 const btnPlay=document.getElementById("btn-play")
 const divOngoing= document.getElementById("ongoing-div")
+const container= document.getElementById("container")
   
   //.style.width = "300px";
+const PRESETS_QUANTITY =6
 let time = 6;
 let totalTime;
 let intervalId;
 let  playing=false;
+  let times=[]
 
 function setup() { 
   song=loadSound('sound.mp3')
  // wave = new p5.Oscillator();
    //wave.setType('sine') 
 }
+window.addEventListener('load',
+ ()=>{  
+   console.log("loaded")
+     if(localStorage.getItem('times')!=undefined){
+    times=JSON.parse(localStorage.getItem("times")) }
 
+     let timesElems= [...new Set(times)].slice(-PRESETS_QUANTITY)
+  console.log(timesElems)
+  for (let i=0; i<timesElems.length; i++){
+    const but =document.createElement("button")
+    but.addEventListener('click',settingPreset)
+    but.innerHTML=timesElems[i]
+   
+          but.classList.add("btn")
+    container.appendChild(but)
+    
+    console.log(timesElems[i])
+  
+  }
+ }
+ )
+
+const settingPreset=(event)=>{
+  time = parseInt(event.target.innerHTML)
+  totalTime = parseInt(event.target.innerHTML)
+  console.log(totalTime)
+  timerText.innerHTML = totalTime
+  storeTime
+}
 const setting= ()=>{ 
   time= parseInt(timerImp.value)
   totalTime = parseInt(timerImp.value)
   timerText.innerHTML=time
-  var times=[]
-  if(localStorage.getItem('times')!=undefined){
-    times=JSON.parse(localStorage.getItem("times"))
-  }
-  
+  storeTime
+}
+
+const storeTime= ()=>{
   times.push(time)
   localStorage.setItem("times",JSON.stringify(times))
-}  
+
+  
+  console.log([...new Set(times)]  )
+
+  }
+  
  function togglePlay(){
   if (!playing){
     //console.log("no")
@@ -46,7 +81,7 @@ const setting= ()=>{
     playing=false;
   }                      
 }
-timerImp.addEventListener('click',                          (e)=>{e.target.value=""})
+timerImp.addEventListener('click',       (e)=>{e.target.value=""})
 
 
 btnSet.addEventListener('click',()=>{setting()})
